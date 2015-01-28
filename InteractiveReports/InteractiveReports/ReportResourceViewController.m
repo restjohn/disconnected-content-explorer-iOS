453d871077cb0012e00fe3c204397f59d923ca3a
stjohnr <stjohnr@bit-sys.com>
2015-01-28 13:15:33 -0700

    hide the status from the app plist instead of using UIViewController:prefersStatusBarHidden to fix the disappearing navigation item in ios 8
    Conflicts:
    	InteractiveReports/InteractiveReports/ReportResourceViewController.m

--- InteractiveReports/InteractiveReports/ReportResourceViewController.m
+++ InteractiveReports/InteractiveReports/ReportResourceViewController.m
@@ -20,27 +20,24 @@
 
 @implementation ReportResourceViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
     
     UIViewController<ResourceHandler> *resourceViewer = [ResourceTypes viewerForResource:self.resource];
     [self addChildViewController:resourceViewer];
     resourceViewer.view.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
-    resourceViewer.view.frame = self.resourceView.bounds;
+    resourceViewer.view.frame = self.resourceView.frame;
     [self.resourceView addSubview:resourceViewer.view];
     [resourceViewer didMoveToParentViewController:self];
     
     [resourceViewer handleResource:self.resource forReport:self.report];
 }
 
-- (void)viewWillDisappear:(BOOL)animated
-{
+- (void)viewWillDisappear:(BOOL)animated {
     [super viewWillDisappear:animated];
 }
 
-- (void)viewDidDisappear:(BOOL)animated
-{
+- (void)viewDidDisappear:(BOOL)animated {
     [super viewDidDisappear:animated];
     
     [self.resourceViewer willMoveToParentViewController:nil];
@@ -49,15 +46,9 @@
     self.resourceViewer = nil;
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
-- (BOOL)prefersStatusBarHidden
-{
-    return YES;
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
@@ -71,8 +62,7 @@
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
 }
